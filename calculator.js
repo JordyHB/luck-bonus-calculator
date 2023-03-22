cd1fd50b4287d05beea16875a6eccd97d4d6d1cf
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-03-22 22:22:20 +0100

    got the script working, need to refine formula

--- calculator.js
+++ calculator.js
@@ -1,11 +1,23 @@
 function returnInput() {
+    // gets the amount of work hours input on the form
     const hoursWorked = document.getElementById('hours-field').value;
+    // get the amount of work minutes from the input form
+    const minWorked = document.getElementById('minutes-field').value;
+    // gets the amount of jobs done input on the form
     const jobsDone = document.getElementById('jobs-field').value;
-    if (jobsDone / hoursWorked > 2.9) {
-        console.log('congrats you were lucky this week')
-    } else {
-        console.log('stinky')
-    }
+    // preps the output field variables
+    const minBonusField = document.getElementById('orders-needed-min')
+    console.log(minBonusField)
+    const midBonusField = document.getElementById('orders-needed-mid')
+    console.log(midBonusField)
+    const maxBonusField = document.getElementById('orders-needed-high')
+    console.log(maxBonusField)
+    minBonusField.innerHTML = (Math.floor(hoursWorked * 2.50)).toString()
+    midBonusField.innerHTML = (Math.floor(hoursWorked * 2.7)).toString()
+    maxBonusField.innerHTML = (Math.floor(hoursWorked * 2.90)).toString()
+
+    event.preventDefault();
 }
 
 console.log('hello')
+
