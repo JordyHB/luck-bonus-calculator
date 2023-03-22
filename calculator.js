b65d69ea272d1f76d92810fdbcc85bd813fe97c6
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-03-22 16:43:56 +0100

    end of code session commit

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
-const hoursWorked = 10;
-const jobsDone = 30
-
-if (hoursWorked / jobsDone >= 2.9) {
-    console.log('congrats you were lucky this week')
+function returnInput() {
+    const hoursWorked = document.getElementById('hours-field').value;
+    const jobsDone = document.getElementById('jobs-field').value;
+    if (jobsDone / hoursWorked > 2.9) {
+        console.log('congrats you were lucky this week')
+    } else {
+        console.log('stinky')
+    }
 }
+
+console.log('hello')
