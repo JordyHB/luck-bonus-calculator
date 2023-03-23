60ee749d8ed0f2f8c939b7b3010813ecf32178a5
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-03-23 16:57:41 +0100

    added the personal result section, added support for work minutes

--- calculator.js
+++ calculator.js
@@ -5,17 +5,25 @@ function returnInput() {
     const minWorked = document.getElementById('minutes-field').value;
     // gets the amount of jobs done input on the form
     const jobsDone = document.getElementById('jobs-field').value;
-    // preps the output field variables
-    const minBonusField = document.getElementById('orders-needed-min')
-    console.log(minBonusField)
-    const midBonusField = document.getElementById('orders-needed-mid')
-    console.log(midBonusField)
-    const maxBonusField = document.getElementById('orders-needed-high')
-    console.log(maxBonusField)
-    minBonusField.innerHTML = (Math.ceil(hoursWorked * 2.4951)).toString()
-    midBonusField.innerHTML = (Math.ceil(hoursWorked * 2.6951)).toString()
-    maxBonusField.innerHTML = (Math.ceil(hoursWorked * 2.8951)).toString()
 
+    // preps the general output field variables
+    const minBonusField = document.getElementById('orders-needed-min')
+    const midBonusField = document.getElementById('orders-needed-mid')
+    const maxBonusField = document.getElementById('orders-needed-high')
+    // multiplies the input of the hours + the decimals of the minutes field by neccesary number to get the different bonuses
+    minBonusField.innerHTML = (Math.ceil((Number(hoursWorked) + Number(minWorked) / 60) * 2.4951)).toString()
+    midBonusField.innerHTML = (Math.ceil((Number(hoursWorked) + Number(minWorked) / 60) * 2.6951)).toString()
+    maxBonusField.innerHTML = (Math.ceil((Number(hoursWorked) + Number(minWorked) / 60) * 2.8951)).toString()
+
+    // preps the personal output field variables
+    const personalScore = document.getElementById('personal-score')
+    const tillMaxScore = document.getElementById('till-max')
+    const tillMidScore = document.getElementById('till-mid')
+    const tillMinScore = document.getElementById('till-min')
+
+    // returns the personal score by dividing orders done by hours + minutes worked
+    personalScore.innerHTML = (Number(jobsDone) / (Number(hoursWorked) + Number(minWorked) / 60)).toFixed(2).toString()
+    // prevents the page refreshing on form submit
     event.preventDefault();
 }
 
